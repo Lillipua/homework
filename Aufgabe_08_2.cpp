3c34595db165ebebed5a5abd1a4dd4509f5cd1ab
Lillipua <45999603+Lillipua@users.noreply.github.com>
2018-12-19 15:03:17 +0100

    Add files via upload

---
+++ Aufgabe_08_2.cpp
@@ -0,0 +1,35 @@
+/*** Aufgabe_08_2.cpp ***/
+#include <iostream>
+using namespace std;
+
+#include "Aufgabe_08_2.h"
+
+int main() {
+    unsigned int const n = 7;
+    int data[] = { 1, 2, 2, -2, 1, -6, 8 };
+
+    Schlange<int> queue1;
+
+    for (unsigned int i = 0; i < n; ++i) {
+        queue1.enq(data[i]);
+    }
+
+    queue1.print();
+
+    // Testaufruf von insertAfter()
+    // Einfuegen nach dem ersten Element
+    queue1.insertAfter(queue1.frontObj(), 3);
+    queue1.print();
+
+    // Testaufruf von manifold()
+    queue1.manifold();
+    queue1.print();
+
+    return 0;
+}
+/*
+1, 2, 2, -2, 1, -6, 8
+1, 2, 2, -2, 1, -6, 8
+1, 2, 2, -2, 1, -6, 8
+*/
+/********* Ende *********/
